6e777dd1bd60fb680921f25752829589e72f7692
ha quang le <winlp4ever@gmail.com>
2019-10-14 15:50:42 +0200

    change style

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Menu from "./components/menu/menu";
+import MdEditor from "./components/mdEditor/mdEditor";
 
 import "./_common.scss";
 
 function renderWeb() {
     ReactDOM.render(<Menu />, document.getElementById("main"));
+    ReactDOM.render(<MdEditor />, document.getElementById("post"));
 }
 renderWeb();
-
-const what = 'what is this thing? oof ';
-console.log('hey you');
 
 if (module.hot) {
     module.hot.accept(['./components/menu/menu'], () => {
