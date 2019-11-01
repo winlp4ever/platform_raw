70367ed79a0d77d4c44353dc4f1ab1a93cddbdaf
Le Ha Quang <winlp4ever@gmail.com>
2019-11-01 23:09:09 +0100

    add discuss

---
+++ src/components/discuss/discuss.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+
+class Discuss extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: [],
+            
+        }
+    }
+}
